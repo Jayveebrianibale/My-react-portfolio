bc80119de44bb42ba28bc94ddf66b44bff0811ed
Jeibii <jayveebriani@gmail.com>
2024-09-29 17:42:53 +0800

    Animations

--- portfolio-app/vite.config.js
+++ portfolio-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'My-react-porfolio',
+  base: 'my-react-porfolio',
   plugins: [react()],
 });
